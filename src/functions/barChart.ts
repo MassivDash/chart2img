4a25f812d4bf19b39c201a1a40d132cb87fb52c8
SpaceGhost <lukasz.celitan@gmail.com>
2024-04-05 11:20:12 +0200

    feat(chart.js): add a dynamic chart maker

--- src/functions/barChart.ts
+++ src/functions/barChart.ts
@@ -1,119 +1,128 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
-import { createCanvas } from 'canvas';
+import {
+  app,
+  HttpRequest,
+  HttpResponseInit,
+  InvocationContext,
+} from "@azure/functions";
+import { createCanvas } from "canvas";
 
-export async function barChart(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
+export async function barChart(
+  request: HttpRequest,
+  context: InvocationContext
+): Promise<HttpResponseInit> {
+  context.log(`Http function processed request for url "${request.url}"`);
 
-        // usual import results in typescript mismatch
-    const Chart = require('chart.js/auto');
+  // usual import results in typescript mismatch
+  const Chart = require("chart.js/auto");
 
-    const width = 400;
-    const height = 400;
+  const width = 400;
+  const height = 400;
 
-    const canvas = createCanvas(width, height);
-    const ctx = canvas.getContext('2d');
+  const canvas = createCanvas(width, height);
+  const ctx = canvas.getContext("2d");
 
-    const plugin = {
-        id: 'customCanvasBackgroundColor',
-        beforeDraw: (chart, _args, options) => {
-          const { ctx } = chart;
-          ctx.save();
-          ctx.globalCompositeOperation = 'destination-over';
-          ctx.fillStyle = options.color || '#99ffff';
-          ctx.fillRect(0, 0, chart.width, chart.height);
-          ctx.restore();
+  const plugin = {
+    id: "customCanvasBackgroundColor",
+    beforeDraw: (chart, _args, options) => {
+      const { ctx } = chart;
+      ctx.save();
+      ctx.globalCompositeOperation = "destination-over";
+      ctx.fillStyle = options.color || "#99ffff";
+      ctx.fillRect(0, 0, chart.width, chart.height);
+      ctx.restore();
+    },
+  };
+
+  const plugins = [plugin];
+
+  const options = {
+    plugins: {
+      datalabels: { display: false },
+      legend: { display: false },
+      customCanvasBackgroundColor: {
+        color: "white",
+      },
+    },
+    indexAxis: "y",
+    responsive: true,
+    scales: {
+      y: {
+        offset: true,
+        ticks: {
+          display: true,
         },
-      };
-    
-      const plugins = [plugin];
-    
-      const options = {
-        plugins: {
-          datalabels: { display: false },
-          legend: { display: false },
-          customCanvasBackgroundColor: {
-            color: 'white',
-          },
+        grid: {
+          display: false,
+          drawTicks: false,
         },
-        indexAxis: 'y',
-        responsive: true,
-        scales: {
-          y: {
-            offset: true,
-            ticks: {
-              display: true,
-            },
-            grid: {
-              display: false,
-              drawTicks: false,
-            },
-            border: {
-              display: false,
-            },
-          },
-          x: {
-            suggestedMin: 0,
-            suggestedMax: 15,
-            border: {
-              display: false,
-            },
-          },
+        border: {
+          display: false,
         },
-      };
+      },
+      x: {
+        suggestedMin: 0,
+        suggestedMax: 15,
+        border: {
+          display: false,
+        },
+      },
+    },
+  };
 
-      const labels = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple', 'Grey'];
-const data = {
-  labels: labels,
-  datasets: [{
-    label: 'My First Dataset',
-    data: [65, 59, 80, 81, 56, 55, 40],
-    backgroundColor: [
-      'rgba(255, 99, 132, 0.2)',
-      'rgba(255, 159, 64, 0.2)',
-      'rgba(255, 205, 86, 0.2)',
-      'rgba(75, 192, 192, 0.2)',
-      'rgba(54, 162, 235, 0.2)',
-      'rgba(153, 102, 255, 0.2)',
-      'rgba(201, 203, 207, 0.2)'
+  const labels = request.query.get("labels")?.split(",");
+  const dataValues = request.query.get("data")?.split(",").map(Number);
+
+  const backgroundColorsRaw = request.query.get("backgroundColors");
+  const borderColorsRaw = request.query.get("borderColors");
+
+  function extractColors(ColorsRaw: string | undefined): string[] | undefined {
+    if (backgroundColorsRaw?.includes("rgba")) {
+      return backgroundColorsRaw.match(/rgba\(([^)]+)\)/g);
+    } else {
+      return backgroundColorsRaw?.split(",");
+    }
+  }
+
+  const backgroundColors = extractColors(backgroundColorsRaw);
+  const borderColors = extractColors(borderColorsRaw);
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: dataValues,
+        backgroundColor: backgroundColors,
+        borderColor: borderColors,
+        borderWidth: 1,
+      },
     ],
-    borderColor: [
-      'rgb(255, 99, 132)',
-      'rgb(255, 159, 64)',
-      'rgb(255, 205, 86)',
-      'rgb(75, 192, 192)',
-      'rgb(54, 162, 235)',
-      'rgb(153, 102, 255)',
-      'rgb(201, 203, 207)'
-    ],
-    borderWidth: 1
-  }]
-};
+  };
 
-    const chartConfig = {
-      type: 'bar',
-      data: data,
-      options: options,
-      plugins
-    };
+  console.log(data);
 
+  const chartConfig = {
+    type: "bar",
+    data: data,
+    options: options,
+    plugins,
+  };
 
-    new Chart(ctx, chartConfig);
+  new Chart(ctx, chartConfig);
 
-    const pngBuffer = canvas.toBuffer('image/png');
+  const pngBuffer = canvas.toBuffer("image/png");
 
-    // Set the headers and send the PNG buffer in the response
-    request.headers.set('Content-Type', 'image/png');
-    request.headers.set('Content-Length', pngBuffer.length.toString());
-    return {
-        status: 200,
-        body: pngBuffer,
-        headers: request.headers
-    };
+  // Set the headers and send the PNG buffer in the response
+  request.headers.set("Content-Type", "image/png");
+  request.headers.set("Content-Length", pngBuffer.length.toString());
+  return {
+    status: 200,
+    body: pngBuffer,
+    headers: request.headers,
+  };
+}
 
-};
-
-app.http('barChart', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: barChart
+app.http("barChart", {
+  methods: ["GET", "POST"],
+  authLevel: "anonymous",
+  handler: barChart,
 });
